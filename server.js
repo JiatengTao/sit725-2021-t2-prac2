15a14c589a2e7e84c6b6c30506e0a1c61d8872b0
JiatengTao <taojia@deakin.edu.au>
2022-03-21 01:17:58 +1100

    allow user signup and signin one account

--- server.js
+++ server.js
@@ -4,22 +4,44 @@ var app= express ()
 
 app.use(express.static(__dirname+'/public'));
 
-var adder=function(num1,num2){
-    var result=num1+num2;
-    return result
+var username;
+var password;
+var signUp=function(input1,input2){
+    username=input1;
+    password=input2;
+}
+var logIn=function(input1,input2){
+    if(input1==username && input2==password){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
-app.get('/test',function(req,res){
+/*app.get('/test',function(req,res){
     var username=req.query.username;
     console.log('hit by '+username);
     res.send("hello! "+username);
+})*/
+
+app.get('/signUp',function(req,res){
+    var input1=req.query.username;
+    var input2=req.query.password;
+    signUp(input1,input2);
+    console.log('Username: '+username+" Password: "+password)
+    res.send('Username: '+username+" Password: "+password)
 })
 
-app.get('/adder',function(req,res){
-    var num1=parseInt(req.query.num1);
-    var num2=parseInt(req.query.num2);
-    var result=adder(num1,num2);
-    res.send('The result is: '+result)
+app.get('/logIn',function(req,res){
+    var input1=req.query.username;
+    var input2=req.query.password;
+    if(logIn(input1,input2)){
+        res.send("success")
+    }
+    else{
+        res.send("fail")
+    }
 })
 
 var port=3000;
